9c7bf97a2e515067078c92aa4ecf8f11aed02e86
Kamijoukirito <806636667@qq.com>
2020-08-31 21:34:53 +0800

    一些基本说明

---
+++ base/battle_filed.py
@@ -0,0 +1,14 @@
+class battle_filed:
+    def __init__(self):
+        self.unit_list = []
+
+    def action(self):
+        for i in self.unit_list:
+            enemy = self.check_collision(i)
+            i.action(enemy)
+
+    def check_collision(self, unit):
+        # 返回相遇对象，否则返回None
+        # 注：这里最好只检查面前，以减少计算量并且避免BUG
+        # 方向可以通过 unit.flag 判断
+        pass
