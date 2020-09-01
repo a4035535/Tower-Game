e84414b1fd065e42df5e264cd28102ac816c98ed
Kamijoukirito <806636667@qq.com>
2020-09-01 14:50:31 +0800

    格式，导入bug修改

--- base/battle_filed.py
+++ base/battle_filed.py
@@ -1,3 +1,5 @@
+from base.base import base
+
 DEFAULT_DISTANCE = 50
 BASE_HP = 500
 POS_LEFT = 0
@@ -7,8 +9,8 @@ POS_RIGHT = 1200
 class battle_filed:
     def __init__(self):
         self.unit_list = []
-		self.base = {'left': base(BASE_HP, POS_LEFT, 'left'), 
-					 'right': base(BASE_HP, POS_RIGHT, 'right')}
+        self.base = {'left': base(BASE_HP, POS_LEFT, 'left'),
+                     'right': base(BASE_HP, POS_RIGHT, 'right')}
 
     def action(self):
         for i in self.unit_list:
@@ -19,10 +21,8 @@ class battle_filed:
         for i in self.unit_list:
             if i.HP <= 0:
                 dead_list.append(i)
-				
-        set(self.unit_list) -= set(dead_list)
-		self.unit_list = list(self.unit_list)
-		
+
+        #删除del
 
     def check_collision(self, unit):
         # 返回相遇对象，否则返回None
@@ -39,17 +39,16 @@ class battle_filed:
                 return b < c < a
 
         for i in self.unit_list:
-			# 判定碰撞
+            # 判定碰撞
             if i is not unit and i.pos[1] == pos[1] \
                     and is_in_range(pos[0], pos[0] + rag, i.pos[0]):
                 return i
-		
-		base_flag = 'left' if 'right' == unit.flag else 'right'
-		# 与基地发生碰撞
-		if self.base[base_flag].pos[1] == pos[1] \ 
-					and is_in_range(pos[0], pos[0] + rag, self.base[base_flag].pos[0]):
-		# 游戏状态 right_win, left_win	
-		game_statue = self.base[base_flag].loss_HP(i)
-		
+
+                base_flag = 'left' if 'right' == unit.flag else 'right'
+                # 与基地发生碰撞
+                if self.base[base_flag].pos[1] == pos[1] \
+                        and is_in_range(pos[0], pos[0] + rag, self.base[base_flag].pos[0]):
+                #游戏状态 right_win, left_win
+                    game_statue = self.base[base_flag].loss_HP(i)
 
         return None, game_statue
