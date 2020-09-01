02bf20decbc3734ea371b9080a26d527f7c5946a
Kamijoukirito <806636667@qq.com>
2020-09-01 10:26:26 +0800

    调整对任意flag适用

--- base/battle_filed.py
+++ base/battle_filed.py
@@ -16,6 +16,8 @@ class battle_filed:
         # 方向可以通过 unit.flag 判断
         pos = unit.pos
 
+        rag = DEFAULT_DISTANCE if unit.flag == 'left' else -DEFAULT_DISTANCE
+
         def is_in_range(a, b, c):
             if a < b:
                 return a < c < b
@@ -24,7 +26,7 @@ class battle_filed:
 
         for i in self.unit_list:
             if i is not unit and i.pos[0] == pos[0] \
-                    and is_in_range(pos[1], pos[1] + DEFAULT_DISTANCE, i.pos[1]):
+                    and is_in_range(pos[1], pos[1] + rag, i.pos[1]):
                 return i
 
         return None
