5074ec98b0e462efe6e143588bbaff123c3f9cfa
Kamijoukirito <806636667@qq.com>
2020-09-01 10:22:37 +0800

    碰撞函数
    基于坐标像素的碰撞函数简单设计

--- base/battle_filed.py
+++ base/battle_filed.py
@@ -1,3 +1,6 @@
+DEFAULT_DISTANCE = 50
+
+
 class battle_filed:
     def __init__(self):
         self.unit_list = []
@@ -11,5 +14,17 @@ class battle_filed:
         # 返回相遇对象，否则返回None
         # 注：这里最好只检查面前，以减少计算量并且避免BUG
         # 方向可以通过 unit.flag 判断
-        pass
+        pos = unit.pos
+
+        def is_in_range(a, b, c):
+            if a < b:
+                return a < c < b
+            else:
+                return b < a < c
+
+        for i in self.unit_list:
+            if i is not unit and i.pos[0] == pos[0] \
+                    and is_in_range(pos[1], pos[1] + DEFAULT_DISTANCE, i.pos[1]):
+                return i
+
         return None
