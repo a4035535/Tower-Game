dbb09bfb139bc6f0cf752e7893e32f6c1b541126
wangshaoping <37463764+17683995446@users.noreply.github.com>
2020-09-02 13:33:38 +0800

    Update map.py

--- base/map.py
+++ base/map.py
@@ -102,6 +102,9 @@ class map:
         self.category=category
         self.road_index=road_index
 
+    def getAttribute(self):
+        return self.road_index,self.category
+
     def isOnclick(self):
         global category
         point_x, point_y = pygame.mouse.get_pos()
