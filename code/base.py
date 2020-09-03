b493778d5a934f4342afdcab42d58db867534e56
wangshaoping <37463764+17683995446@users.noreply.github.com>
2020-09-03 14:47:30 +0800

    Revert "Merge branch 'master' into wspnew"
    This reverts commit 6fc8efb743a618d016d83ae3cc033dbd500d06b3, reversing
    changes made to 398b413fe957bfebc3473280c7571097990d60ad.

--- code/base.py
+++ code/base.py
@@ -5,11 +5,11 @@ class base:
         self.flag = flag
 
     def loss_HP(self, enemy):
-        self.HP -= 100
+        self.HP -= enemy.ATK
         if self.HP <= 0:
             if self.flag == 'right':
                 return 'left_win'
             else:
                 return 'right_win'
         else:
-            return 'running'
+            pass
--- code/map.py
+++ code/map.py
@@ -189,4 +189,3 @@ if __name__ == "__main__":
         game.displaySoldiers(unit_lists,base_hp)
         game.load_menu(now_cd, max_cd)
         pygame.display.update()
-        print(game_statue)
